7d775745803a2f69726f3bb2c7adb846d2afa7f2
Alex Sobolev <31108@mail.ru>
2022-08-31 19:55:40 +0300

    Setup DataManager

--- HW2.9 WorkWithAnimation/DataManager.swift
+++ HW2.9 WorkWithAnimation/DataManager.swift
@@ -5,4 +5,43 @@
 //  Created by Александр Соболев on 31.08.2022.
 //
 
-import Foundation
+import Spring
+
+class DataManager {
+    
+    static let shared = DataManager()
+    
+    let animations: [Spring.AnimationPreset] = [
+        .fadeIn,
+        .fadeInDown,
+        .fadeInLeft,
+        .fadeInRight,
+        .fadeInUp,
+        .fadeOut,
+        .fadeOutIn,
+        .fall,
+        .flash,
+        .flipX,
+        .flipY,
+        .morph,
+        .pop,
+        .shake,
+        .slideDown,
+        .slideLeft,
+        .slideRight,
+        .slideUp,
+        .squeeze,
+        .squeezeDown,
+        .squeezeLeft,
+        .squeezeRight,
+        .squeezeUp,
+        .swing,
+        .zoomIn,
+        .wobble,
+        .zoomOut
+    ]
+    
+    let curves = Spring.AnimationCurve.allCases
+    
+    private init() {}
+}
