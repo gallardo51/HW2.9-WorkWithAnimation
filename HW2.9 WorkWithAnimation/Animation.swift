76f9536e3d9b07e1aa4a6a41400431acb4409d52
Alex Sobolev <31108@mail.ru>
2022-08-31 19:57:38 +0300

    Create struct Animation

--- HW2.9 WorkWithAnimation/Animation.swift
+++ HW2.9 WorkWithAnimation/Animation.swift
@@ -5,4 +5,29 @@
 //  Created by Александр Соболев on 31.08.2022.
 //
 
-import Foundation
+struct Animation {
+    
+    let name: String
+    let curve: String
+    let force: Float
+    let delay: Float
+    
+    var description: String {
+        """
+        preset: \(name)
+        curve: \(curve)
+        force: \(String(format: "%.02f", force))
+        delay: \(String(format: "%.02f", delay))
+        """
+    }
+    
+    static func getRandomAnimation() -> Animation {
+        Animation(
+            name: DataManager.shared.animations.randomElement()?.rawValue ?? "slideLeft",
+            curve: DataManager.shared.curves.randomElement()?.rawValue ?? "easeIn",
+            force: Float.random(in: 1...2),
+            delay: 0.3
+        )
+    }
+}
+
