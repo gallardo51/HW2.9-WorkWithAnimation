69b881e4ce86b779f6e0a2065ad904ea8ca8590b
Alex Sobolev <31108@mail.ru>
2022-08-31 14:51:00 +0300

    Initial Commit

---
+++ HW2.9 WorkWithAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.9 WorkWithAnimation
+//
+//  Created by Александр Соболев on 31.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
